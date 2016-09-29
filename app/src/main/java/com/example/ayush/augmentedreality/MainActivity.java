b6674bc092d25d1ecb4a6b6e191821034f5ac53b
Ayush Vijay <iitg.ayush@gmail.com>
2016-09-29 18:15:58 +0530

    Completed Login and Signup using Firebase!
    Reference: Step "Security and Rules" 
    https://www.sitepoint.com/creating-a-cloud-backend-for-your-android-app-using-firebase/

--- app/src/main/java/com/example/ayush/augmentedreality/MainActivity.java
+++ app/src/main/java/com/example/ayush/augmentedreality/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ayush.augmentedreality;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null) {
+            // Not logged in, launch the Log In activity
+            loadLogInView();
+        }
+
 
 
     }
@@ -37,10 +55,19 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
-            return true;
+            mFirebaseAuth.signOut();
+            loadLogInView();
         }
 
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void loadLogInView() {
+        Intent intent = new Intent(this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
