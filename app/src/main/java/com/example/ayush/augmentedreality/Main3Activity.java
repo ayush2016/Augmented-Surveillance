6f204ad761a0aeb8c3afeff6ff01d7ee4a5a148e
Ayush Vijay <iitg.ayush@gmail.com>
2016-10-07 18:46:30 +0530

    Location Data Firebase Integration Done!

--- app/src/main/java/com/example/ayush/augmentedreality/Main3Activity.java
+++ app/src/main/java/com/example/ayush/augmentedreality/Main3Activity.java
@@ -4,7 +4,6 @@ import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -14,12 +13,33 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+import static com.example.ayush.augmentedreality.R.id.latitude;
+import static com.example.ayush.augmentedreality.R.id.longitude;
 
 public class Main3Activity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
@@ -31,6 +51,12 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
     GoogleMap mGoogleMap;
     // SupportMapFragment mFragment;
     Marker currLocationMarker;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private String mUserId;
+    private DatabaseReference myFirebaseRef=FirebaseDatabase.getInstance().getReference();
+    List<Marker> markerList = new ArrayList<>();
+    public static final int MAP_ZOOM_LEVEL = 7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,15 +65,18 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+      //  mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserId = mFirebaseUser.getUid();
     }
 
     @Override
     public void onMapReady(GoogleMap gMap) {
         mGoogleMap = gMap;
         mGoogleMap.setMyLocationEnabled(true);
-
         buildGoogleApiClient();
-
         mGoogleApiClient.connect();
 
     }
@@ -84,9 +113,6 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
         //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
 
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
-
-
     }
 
     @Override
@@ -99,8 +125,18 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
         Toast.makeText(this,"onConnectionFailed",Toast.LENGTH_SHORT).show();
     }
 
+    Location mCurrentLocation;
+    private String mLastUpdateTime;
+
     @Override
     public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = new Date();
+        mLastUpdateTime = dateFormat.format(date).toString();
+
 
         //place marker at current position
         //mGoogleMap.clear();
@@ -111,7 +147,7 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.title("Current Position");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.army));
         currLocationMarker = mGoogleMap.addMarker(markerOptions);
 
         Toast.makeText(this,"Location Changed", Toast.LENGTH_SHORT).show();
@@ -125,8 +161,77 @@ public class Main3Activity extends AppCompatActivity implements OnMapReadyCallba
 
         //If you only need one location, unregister the listener
         //LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-
+        saveToFirebase();
+        // Retrieve saved locations and draw as marker on map
+        drawLocations();
     }
 
+    private void saveToFirebase() {
+        Map mLocations = new HashMap();
+        mLocations.put("timestamp", mLastUpdateTime);
+        Map  mCoordinate = new HashMap();
+        mCoordinate.put("latitude", mCurrentLocation.getLatitude());
+        mCoordinate.put("longitude", mCurrentLocation.getLongitude());
+        mLocations.put("location", mCoordinate);
+        myFirebaseRef.child("users").child(mUserId).push().setValue(mLocations);
+    }
+
+    private void drawLocations() {
+        // Get only latest logged locations - since 'START' button clicked
+        Query queryRef = myFirebaseRef.child("users").child(mUserId).orderByChild("timestamp");
+        // Add listener for a child added at the data at this location
+        queryRef.addChildEventListener(new ChildEventListener() {
+            LatLngBounds bounds;
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map  data = (Map ) dataSnapshot.getValue();
+                String timestamp = (String) data.get("timestamp");
+                // Get recorded latitude and longitude
+                Map  mCoordinate = (HashMap)data.get("location");
+                double latitude = (double) (mCoordinate.get("latitude"));
+                double longitude = (double) (mCoordinate.get("longitude"));
+
+                // Create LatLng for each locations
+                LatLng mLatlng = new LatLng(latitude, longitude);
+
+                // Make sure the map boundary contains the location
+                builder.include(mLatlng);
+                bounds = builder.build();
+
+                // Add a marker for each logged location
+                MarkerOptions mMarkerOption = new MarkerOptions()
+                        .position(mLatlng)
+                        .title(mUserId + timestamp)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.army));
+                Marker mMarker = mGoogleMap.addMarker(mMarkerOption);
+                markerList.add(mMarker);
+
+                // Zoom map to the boundary that contains every logged location
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds,
+                        MAP_ZOOM_LEVEL));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+               // adapter.remove((String) dataSnapshot.child("title").getValue());
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
 
