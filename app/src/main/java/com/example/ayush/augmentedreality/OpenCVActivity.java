32902532fd14ca9891a2334a0d70e687423ffe2f
Ayush Vijay <iitg.ayush@gmail.com>
2017-03-31 10:37:21 +0530

    setMaxFrameSize(640,480)

--- app/src/main/java/com/example/ayush/augmentedreality/OpenCVActivity.java
+++ app/src/main/java/com/example/ayush/augmentedreality/OpenCVActivity.java
@@ -47,6 +47,7 @@ public class OpenCVActivity extends AppCompatActivity implements CameraBridgeVie
         javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(this);
+        javaCameraView.setMaxFrameSize(640,480);
     }
 
     @Override
